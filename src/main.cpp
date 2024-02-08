85f44aac6f432efe3e5dcc205a5797519ad7f100
Josep Mengual <josep@truita.es>
2024-02-08 20:26:00 +0100

    Servo controller

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,15 @@
 #define MPU_ADDRESS 0x68
 #define MPU_CALIBRATION_ITERATIONS 1000
 
+#define DIR_KP 2
+
 MPU9250 mpu;
 HardwareSerial hs(1);
 
 float current_direction = 0.0f;
 unsigned long gyro_last_time = 0;
 float gyro_offset;
+float target_angle = 0.0f;
 
 void motorSpeed(int speed) {
   hs.print("M");
@@ -30,7 +33,7 @@ void calibrateImu() {
   gyro_offset = totalAngle / MPU_CALIBRATION_ITERATIONS;
 }
 
-void updateGyro() {
+bool updateGyro() {
   if(mpu.update()) {
     long current_millis = millis();
     long gyro_dt = current_millis - gyro_last_time;
@@ -38,23 +41,32 @@ void updateGyro() {
     float gyro_value = mpu.getGyroZ() - gyro_offset;
     current_direction += gyro_value * gyro_dt / 1000;
     Serial.println(current_direction);
+    return true;
   }
+  return false;
 }
 
 void setup() {
   // Motor
   hs.begin(112500, SERIAL_8N1, 4, 2);
-  motorSpeed(90);
-  servoAngle(90);
 
   Wire.begin();
   mpu.setup(MPU_ADDRESS);
   calibrateImu();
 
+  motorSpeed(15);
+  servoAngle(90);
+
   //Debugging
   Serial.begin(9600);
+  gyro_last_time = millis();
 }
 
 void loop() {
-  updateGyro();
+  if(updateGyro()) {
+    servoAngle((target_angle - current_direction) * DIR_KP + 90);
+  }
+  if(millis() > 10000) {
+    target_angle = 90;
+  }
 }
