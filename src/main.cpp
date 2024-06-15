cc8ddaa94545cf6ac82a6318401809b0c33c4f3c
Daniel Kauss <daniel.kauss.serna@gmail.com>
2024-06-15 06:43:46 +0200

    Cam fix and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,45 +10,45 @@
 
 #define SIMULATE_MOVEMENT
 
-vector2_t position = { .x = 0, .y = 0 };
 PID servoPid(0.5f, 0.08f, 0.35f);
 Imu imu;
 Timer nav_timer(20);
 
+#define ENCODERS_TO_MM 1.6f
+vector2_t position = { .x = 0, .y = 0 };
 void updatePosition(vector2_t *pos, float angle, int encoders) {
-  pos->x += encoders * cos(angle) * 1.6;
-  pos->y += encoders * sin(angle) * 1.6;
-  //debug_msg("X: %f, Y: %f", pos->x, pos->y);
+  pos->x += encoders * cos(angle) * ENCODERS_TO_MM;
+  pos->y += encoders * sin(angle) * ENCODERS_TO_MM;
   debug_position(*pos);
 }
 
+#define ERROR_COEFICENT 0.005f
+#define ANGLE_CONSTRAINT PI / 3
 float angleToAxis(float from, float to) {
-  float angle = (to - from) * 0.005;
-  return constrain(angle, -(PI / 2), (PI / 2));
+  float angle = (to - from) * ERROR_COEFICENT;
+  return constrain(angle, -ANGLE_CONSTRAINT, ANGLE_CONSTRAINT);
 }
 
 int turn_count = 0;
 
-#define BLOCK_FIXED_OFFSET 200
+#define BLOCK_FIXED_OFFSET 250
 #define MIN_AFTER_DISTANCE 200
 float last_block = 0;  // last recorded position of the block
 float cam_offset = 0;
 void camOffsetGood(int zone) {
   float pos = (turn_count % 2) ? position.y : position.x;
   if (green_block.in_scene) {
-    cam_offset = BLOCK_FIXED_OFFSET;
+    if (cam_offset == 0) { debug_msg("green block"); }
+    cam_offset = BLOCK_FIXED_OFFSET;  // left
     last_block = pos;
   } else if (red_block.in_scene) {
-    cam_offset = -BLOCK_FIXED_OFFSET;
+    if (cam_offset == 0) { debug_msg("red block"); }
+    cam_offset = -BLOCK_FIXED_OFFSET;  // right
     last_block = pos;
   }
 
   if (abs(last_block - pos) > MIN_AFTER_DISTANCE) {
     cam_offset = 0;
-  }
-
-  if (cam_offset != 0) {
-    debug_msg("cam_offset until: %f", 100 - abs(last_block - pos));
   }
 }
 
@@ -79,7 +79,7 @@ float zoneGood() {
   }
 
   camOffsetGood(zone);
-  to += cam_offset * sign;
+  to += cam_offset * sign * orientation;
 
   float angle = turn_count * (PI / 2) + (angleToAxis(from, to) * sign);
 
@@ -94,16 +94,16 @@ float zoneGood() {
 }
 
 float start_distance_y;
-
 void setup() {
   pinMode(33, INPUT_PULLUP);
   pinMode(26, OUTPUT);
 
-  delay(200);
   debug_init();
   slaveSetup();
-  slaveProcessSerial();
   imu.setup();
+
+  while (battery < 0.01f) { slaveProcessSerial(); }  // WARN
+  debug_battery(battery);
 
   if (battery > 4) {
     lidarSetup();
@@ -115,60 +115,49 @@ void setup() {
   }
   debug_msg("Setup completed");
 
-  // // Wait for the user to press the start button
-  // digitalWrite(26, HIGH);
-  // while (digitalRead(33)) {
-  // }
-  // digitalWrite(26, LOW);
-  // delay(500); // Some time for the user to get their finger out of the way,
-  //             // otherwise their finger could easily be cut off in a very
-  //             // awful way. Not recommended.
+  // wait for button
+  digitalWrite(26, HIGH);
+  while (digitalRead(33)) {
+  }
+  digitalWrite(26, LOW);
+  delay(500);
+
+  if (battery < 4) {
+    motorSpeed(0);
+  } else {
+    motorSpeed(150);
+  }
+
   servoPid.target = 0;
 }
 
-Timer batteryTimer(1000);
-
 void loop() {
-  if (batteryTimer.primed()) {
-    if (battery < 4) {
-      motorSpeed(0);
-    } else {
-      motorSpeed(350);
-    }
-    debug_battery(battery);
-  }
-
   slaveProcessSerial();
-#ifdef SIMULATE_MOVEMENT
-  debug_current_direction(imu.rotation);
-#else
+#ifndef SIMULATE_MOVEMENT
   if (imu.update()) {
     debug_current_direction(imu.rotation);
   }
 #endif
 
   if (nav_timer.primed()) {
-#ifdef SIMULATE_MOVEMENT
-    orientation = -1;
-    updatePosition(&position, imu.rotation, 4);
-	imu.rotation = servoPid.target;
-    /* imu.rotation += servoPid.update(imu.rotation) * 0.04; */
-#else
+#ifndef SIMULATE_MOVEMENT
     updatePosition(&position, imu.rotation, getEncoders());
+#else
+    orientation = 1;
+    updatePosition(&position, servoPid.target, 4);
+    debug_current_direction(servoPid.target);
 #endif
 
     // Initial Turn
-    /* if (orientation == 0) { */
-	orientation = 1;
-    if (false) {
+    if (orientation == 0) {
       if (left_distance > 1500) {
+        debug_msg("setting orientation to %i", orientation);
         orientation = 1;
-        //position.y = 500 - start_distance_y;
-        zoneGood();
+        position.y = 500 - start_distance_y;
       } else if (right_distance > 1500) {
+        debug_msg("setting orientation to %i", orientation);
         orientation = -1;
-        //position.y = start_distance_y - 500;
-        zoneGood();
+        position.y = start_distance_y - 500;
       }
     }
     // Standard Navigation
